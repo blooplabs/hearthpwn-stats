1d2294f69c2c4e58578e4add4dd517cbd6bbab99
Andrew Liu <aliuy8@gmail.com>
2016-12-09 18:19:21 -0800

    update for latest website - 12.9.16

--- hearthpwn.py
+++ hearthpwn.py
@@ -8,11 +8,14 @@ class HearthPwnSpider(scrapy.Spider):
 
 
   def parse(self, response):
+    yield scrapy.Request('http://www.hearthpwn.com/cards?display=1&filter-dust-cost-op=1&filter-dust-cost-val=0&filter-rarity=32&filter-show-standard=y', callback=self.parse_listing)
+
+  def parse_listing(self, response):
     # scrape all cards
     for card in response.css("table.listing tbody a[href*='cards']::attr(href)"):
       card_url = response.urljoin(card.extract())
       yield scrapy.Request(card_url, callback=self.parse_card)
-   
+
     # [debug] scrape one card  
     '''
     card = response.css("table.listing tbody a[href*='cards']::attr(href)")
@@ -22,12 +25,19 @@ class HearthPwnSpider(scrapy.Spider):
 
   def parse_card(self, response):
     infobox = response.css(".card-details .infobox::text").extract()
-    usage = filter(lambda x: "Used in" in x, infobox)
+    usage = list(filter(lambda x: "Used in" in x, infobox))
     usage = re.sub("\s+", " ", usage[0]).strip()
+    
+    usage_tokens = usage.split(" ")
+    usage_pct = usage_tokens[2]
+    usage_class = usage_tokens[4]
 
     yield {
       'title' : response.css(".card-details .caption::text").extract()[0],
       'set'   : response.css(".card-details .infobox li a[href*='filter-set']::text").extract()[0],
       'usage' : usage,
+      "usage_pct" : usage_pct,
+      "usage_class" : usage_class,
+
     }
 
